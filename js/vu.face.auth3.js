b5d9e4192d48be9747239791928099e7be030295
laurazorro <56643359+laurazorro@users.noreply.github.com>
2021-06-15 07:15:54 -0500

    ajuste de color SOP

--- js/vu.face.auth3.js
+++ js/vu.face.auth3.js
@@ -93,7 +93,7 @@ vu.face.auth.load = async function(basePath) {
         document.getElementById('vu.sop.ui.userName').placeholder = vu.sop.msg.userInputPlaceholder
         document.getElementById('vu.sop.ui.userNameSendBtn').innerHTML = vu.sop.msg.userSendBtn
 
-        vu.sop.ui.bottomTextBackGroundColor("rgba(255, 102, 0, 1)");
+        vu.sop.ui.bottomTextBackGroundColor("rgba(255, 102, 0, 0.5)");
         await vu.face.auth.createLoadingImg();
 
     } catch (e) {
